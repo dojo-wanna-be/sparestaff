fd68b6557008021e4cc9710cef431e09a9c208be
Mohd Sameer <mohsameer1310@gmail.com>
2019-10-03 11:38:12 +0530

    fix mailer functionality

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,24 @@
+class UserMailer < ApplicationMailer
+ 
+ default :from => "sparestaff.com"
+  def listing_create_confirmation (user)
+	  @user = user
+		mail(to: @user.email, subject: "Please approved listing")
+  end
+
+  def admin_listing_confirmation (user_admin)
+   admin = user_admin.pluck(:email)
+ 	 @admin = admin
+ 	 mail(to: @admin, subject: "Please approved listing")
+  end
+
+  def tfn_verification(user)
+  	@user = user
+  	mail(to: @user.email, subject: "Please Add Tax File Number(TFN)")
+  end
+
+  def photo_verification (user)
+  	@user = user 
+  	mail(to: @user.email, subject: "Please approved listing")
+  end
+end
