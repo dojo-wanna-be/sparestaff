97b20bfeb1b8031f90fe1b3965135e80454dafd6
Mohd Sameer <mohsameer1310@gmail.com>
2019-10-09 10:12:18 +0530

    fix js in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
   def index
     @user = User.new
   end
+
+  # def email_availability
+  #   binding.pry
+  #  @email = User.pluck(:email)
+	 #  respond_to do |format|
+	 #    format.json { render :json => !@email}
+	 #  end 
+  # end
 end
