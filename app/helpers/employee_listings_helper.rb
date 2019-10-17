9a11cdd2799ab92d23c6970db8fbf87e9f5613b1
mohdsameer <mohdsameer1310@gmail.com>
2019-10-17 12:31:56 +0530

    Resolved given issues

--- app/helpers/employee_listings_helper.rb
+++ app/helpers/employee_listings_helper.rb
@@ -4,13 +4,13 @@ module EmployeeListingsHelper
   end
 
   def selected_start_time(listing, day)
-    availablity = listing.listing_availabilities.find_by(day: day)
-    availablity.present? ? availablity.start_time.strftime("%H:%M") : ""
+    availability = listing.listing_availabilities.find_by(day: day)
+    availability.present? && availability.start_time.present? ? availability.start_time.strftime("%H:%M") : ""
   end
 
   def selected_end_time(listing, day)
-    availablity = listing.listing_availabilities.find_by(day: day)
-    availablity.present? ? availablity.end_time.strftime("%H:%M") : ""
+    availability = listing.listing_availabilities.find_by(day: day)
+    availability.present? && availability.end_time.present? ? availability.end_time.strftime("%H:%M") : ""
   end
 
   def disable(listing, day)
@@ -19,5 +19,16 @@ module EmployeeListingsHelper
     else
       ''
     end
+  end
+
+  def listing_images(listing)
+    image_urls = []
+    image_urls << listing.profile_picture.url if listing.profile_picture.present?
+    if listing.relevant_documents.present?
+      listing.relevant_documents.each do |relevant_document|
+        image_urls << relevant_document.document.url
+      end
+    end
+    return image_urls
   end
 end
