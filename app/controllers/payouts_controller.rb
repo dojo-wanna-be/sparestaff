43c1ecb49cb0554c19b218b65d024b1be3ceb2c8
Mohd Sameer <mohsameer1310@gmail.com>
2019-11-21 18:09:53 +0530

    add service for create customer

--- app/controllers/payouts_controller.rb
+++ app/controllers/payouts_controller.rb
@@ -10,4 +10,16 @@ class PayoutsController < ApplicationController
 			StripeAccount.new(params, current_user).create
 		end
 	end
+
+
+	def update
+    new_card = params[:stripeToken]
+    begin
+      last = AddNewCardOnStripe.new(user: current_user, new_card: params[:stripeToken]).update
+      # current_user.update(last_4_of_card_on_file: last.last4, card_type: last.brand)
+      redirect_to root_path
+    rescue
+      redirect_to root_path
+    end
+  end
 end
