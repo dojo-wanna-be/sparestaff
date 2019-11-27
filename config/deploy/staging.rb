c0645c138bbcdd1cc9e07b2916b1cad5e78a3f4f
mohdsameer <mohdsameer1310@gmail.com>
2019-11-27 15:43:45 +0530

    change

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,7 @@
 
 server '13.54.230.136', user: 'ubuntu', roles: %w{app db web}
 
-config.middleware.use ExceptionNotification::Rack,
+Rails.application.config.middleware.use ExceptionNotification::Rack,
 	email: {
 	  deliver_with: :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
 	  email_prefix: "[Error] ",
