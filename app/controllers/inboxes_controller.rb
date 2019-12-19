b13e125a6766557064d8c74e1472fb8b77b74ce4
mohdsameer <mohdsameer1310@gmail.com>
2019-12-19 17:08:48 +0530

    add message functionality

--- app/controllers/inboxes_controller.rb
+++ app/controllers/inboxes_controller.rb
@@ -1,19 +1,31 @@
 class InboxesController < ApplicationController
-  before_action :find_conversation, only: [:show]
+  before_action :find_conversation, only: [:show, :create]
 
   def index
-    @conversations = Conversation.where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id)
+    @conversations = Conversation.includes(:messages).where("conversations.sender_id = ? OR conversations.receiver_id = ?", current_user.id, current_user.id)
   end
 
   def show
-    @transaction = Transaction.find_by(id: params[:id])
-    @listing = @transaction.employee_listing
-    @messages = @conversation.messages.order(created_at: :DESC)
+    if(params[:from].present?)
+      @listing = EmployeeListing.find(params[:id])
+      @conversation = Conversation.create!( receiver_id: @listing.poster.id,
+                    sender_id: current_user.id,
+                    employee_listing_id: @listing.id
+                  )
+      @messages = []
+      redirect_to inbox_path(id: @conversation.id) 
+    else
+      @transaction = @conversation.employee_listing_transaction
+      @listing = @conversation.employee_listing
+      @messages = @conversation.messages.order(created_at: :DESC)
+    end
   end
 
   def create
-    #binding.pry
-    #message = @conversation.messages.new(content: params[:message], sender_id: current_user.id)
+    message = @conversation.messages.new(content: params[:message][:content], sender_id: current_user.id)
+    if message.save
+      @messages = @conversation.messages.order(created_at: :DESC)
+    end
   end
 
   private
@@ -21,4 +33,5 @@ class InboxesController < ApplicationController
   def find_conversation
     @conversation = Conversation.find_by(id: params[:id])
   end
+  
 end
