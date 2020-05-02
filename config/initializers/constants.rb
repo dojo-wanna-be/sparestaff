65acf97cd32faee686a2021217822fcb7f3b225b
Mohd Sameer <mohdsameer1310@gmail.com>
2020-05-02 15:32:51 +0530

    check fb login

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,2 @@
-FACEBOOK_APP_ID     = 661126381117576#Rails.application.credentials[Rails.env.to_sym][:facebook_app_id]
-FACEBOOK_SECRET_KEY = "54af7a098dd4f3fdc4a5ef444041d537"#Rails.application.credentials[Rails.env.to_sym][:facebook_secret_key]
+FACEBOOK_APP_ID     = Rails.application.credentials[Rails.env.to_sym][:facebook_app_id]
+FACEBOOK_SECRET_KEY = Rails.application.credentials[Rails.env.to_sym][:facebook_secret_key]
