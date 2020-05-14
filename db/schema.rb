1d6da8e2fc541552d6975bea8029be19801b2d2c
Mohd Sameer <mohdsameer1310@gmail.com>
2020-05-14 16:09:48 +0530

    feedback changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_073350) do
+ActiveRecord::Schema.define(version: 2020_05_09_090303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,8 @@ ActiveRecord::Schema.define(version: 2020_05_08_073350) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "transaction_id"
+    t.boolean "is_disallowed", default: false
+    t.datetime "deleted_at"
   end
 
   create_table "coupons", force: :cascade do |t|
@@ -265,6 +267,14 @@ ActiveRecord::Schema.define(version: 2020_05_08_073350) do
     t.boolean "read", default: false
     t.integer "deleted_by"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+  end
+
+  create_table "notification_settings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.json "preferences", default: {}
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notification_settings_on_user_id"
   end
 
   create_table "payment_receipts", force: :cascade do |t|
