ad137f8c7d469ab8d08e5404769c79a8f5c33c0b
Mohd Sameer <mohdsameer1310@gmail.com>
2020-05-19 15:58:23 +0530

    issues

--- app/controllers/employee_listings_controller.rb
+++ app/controllers/employee_listings_controller.rb
@@ -304,9 +304,9 @@ class EmployeeListingsController < ApplicationController
       end
     else
       similar_class_listings = @employee_listing.classification&.employee_listings.ransack(title_cont_any: @employee_listing.title.split(" ")).result
-      similar_price_listings = similar_class_listings.ransack(weekday_price_gteq: @employee_listing.weekday_price.to_f - 10, weekday_price_lteq: @employee_listing.weekday_price.to_f + 10).result
-      similar_rank_listings = similar_price_listings.ransack(rating_count_eq: @employee_listing.rating_count).result
-      @similar_nearby_listings = similar_rank_listings.near(@employee_listing.city + ", " + @employee_listing.state + ", " + @employee_listing.country)
+      similar_price_listings = similar_class_listings&.ransack(weekday_price_gteq: @employee_listing.weekday_price.to_f - 10, weekday_price_lteq: @employee_listing.weekday_price.to_f + 10).result
+      similar_rank_listings = similar_price_listings&.ransack(rating_count_eq: @employee_listing.rating_count).result
+      @similar_nearby_listings = similar_rank_listings&.near(@employee_listing.city + ", " + @employee_listing.state + ", " + @employee_listing.country)
       @start_date = @employee_listing.start_publish_date > Date.today ? @employee_listing.start_publish_date : Date.today
       @end_date = @start_date
 
