9a0f18093ef693e2e2d0b8aa5330efa92379d228
Mohd Sameer <mohdsameer1310@gmail.com>
2020-05-19 16:09:28 +0530

    issues

--- app/controllers/employee_listings_controller.rb
+++ app/controllers/employee_listings_controller.rb
@@ -303,7 +303,7 @@ class EmployeeListingsController < ApplicationController
         redirect_to preview_employee_path(id: @employee_listing.id)
       end
     else
-      similar_class_listings = @employee_listing.classification&.employee_listings.ransack(title_cont_any: @employee_listing.title.split(" ")).result
+      similar_class_listings = @employee_listing.classification&.employee_listings&.ransack(title_cont_any: @employee_listing.title.split(" ")).result
       similar_price_listings = similar_class_listings&.ransack(weekday_price_gteq: @employee_listing.weekday_price.to_f - 10, weekday_price_lteq: @employee_listing.weekday_price.to_f + 10).result
       similar_rank_listings = similar_price_listings&.ransack(rating_count_eq: @employee_listing.rating_count).result
       @similar_nearby_listings = similar_rank_listings&.near(@employee_listing.city + ", " + @employee_listing.state + ", " + @employee_listing.country)
