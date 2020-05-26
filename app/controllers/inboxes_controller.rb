7fed5b71e8d636b3688a45ec708edebb269b289f
Mohd Sameer <mohdsameer1310@gmail.com>
2020-05-26 18:06:00 +0530

    recheck

--- app/controllers/inboxes_controller.rb
+++ app/controllers/inboxes_controller.rb
@@ -25,7 +25,7 @@ class InboxesController < ApplicationController
           redirect_to inbox_path(id: @conversation.id)
         rescue => e
           flash[:error] = e.message
-          redirect_to employee_listing_path(id: @listing.id)
+          redirect_to employee_path(id: @listing.id)
         end
       end
     else
