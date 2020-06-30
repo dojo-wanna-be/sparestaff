ebb0a784eececbe493ea9d46ea27f8b0d06484db
Mohd Sameer <mohdsameer1310@gmail.com>
2020-06-30 17:33:22 +0530

    past cancel arranfe

--- app/controllers/hirings_controller.rb
+++ app/controllers/hirings_controller.rb
@@ -32,8 +32,9 @@ class HiringsController < ApplicationController
 
   def index
     hirer_transactions = Transaction.where(hirer_id: current_user.id).order(updated_at: :desc)
-    @hired_listing_transactions = hirer_transactions.where(state: [:created, :accepted, :cancelled]).includes(:employee_listing)
-    @past_listing_transactions = hirer_transactions.where(state: [:expired, :completed]).includes(:employee_listing)
+    @hired_listing_transactions = hirer_transactions.where("end_date >= ?", Date.today).where(state: [:created, :accepted, :cancelled]).includes(:employee_listing)
+    past_cancelled_transactions = hirer_transactions.where(state: [:cancelled]).where("end_date < ?", Date.today).includes(:employee_listing)
+    @past_listing_transactions = past_cancelled_transactions + hirer_transactions.where(state: [:expired, :completed]).includes(:employee_listing)
     @declined_listing_transactions = hirer_transactions.where(state: "rejected").includes(:employee_listing)
   end
 
