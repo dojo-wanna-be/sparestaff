a7549023b2aae8cd16afd3228b68c4adee205a32
Mohd Sameer <mohdsameer1310@gmail.com>
2020-07-20 12:46:39 +0530

    fix indentation

--- app/controllers/stripe_webhook_controller.rb
+++ app/controllers/stripe_webhook_controller.rb
@@ -1,13 +1,11 @@
 class StripeWebhookController < ApplicationController
+  skip_before_action :authenticate_user!
+  protect_from_forgery :except => :handle_stripe_webhook
 
-	skip_before_action :authenticate_user!
-	protect_from_forgery :except => :handle_stripe_webhook
-
-	def handle_stripe_webhook
-		StripeWebhook.handle_webhook(params)
-		respond_to do |format|
-     	format.html { head :ok }
-   	end
-	end
-	 
+  def handle_stripe_webhook
+    StripeWebhook.handle_webhook(params)
+    respond_to do |format|
+      format.html { head :ok }
+    end
+  end
 end
